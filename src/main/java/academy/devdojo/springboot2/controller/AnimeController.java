9dd715b1cfc5ddf35cefbd9b6f47597333d6df34
Marcelo Melo <marcelo.melo@e-core.com>
2022-09-29 16:36:55 -0300

    Initial commit

---
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -0,0 +1,44 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("animes")
+@Log4j2
+@AllArgsConstructor
+public class AnimeController {
+    private DateUtil dateUtil;
+    private AnimeService animeService;
+
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable long id) {
+        animeService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
